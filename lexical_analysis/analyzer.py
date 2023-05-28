4e1762df75b4cb94b6ed916f3c8059d7fb20f000
atom-rad <113429347+atom-rad@users.noreply.github.com>
2023-05-28 12:25:40 +0300

    Update analyzer.py

--- lexical_analysis/analyzer.py
+++ lexical_analysis/analyzer.py
@@ -1,113 +1,49 @@
 import re
 
+
 class Lexer:
-    
-    def __init__(self):
-        # Define some regular expressions for our lexer
-        self.TOKENS = [
-            ('INTEGER', r'\d+'),
-            ('PLUS', r'\+'),
-            ('MINUS', r'\-'),
-            ('MULTIPLY', r'\*'),
-            ('DIVIDE', r'\/'),
-            ('LPAREN', r'\('),
-            ('RPAREN', r'\)'),
-        ]
+    def __init__(self, src_code):
+        self.src_code = src_code
 
-    # A lexer function that takes a string and returns a list of tokens
-    def lexer(self, input_string):
+    def tokenize(self):
+        # This list will keep all tokens
         tokens = []
-        while len(input_string) > 0:
-            match = None
-            for token in TOKENS:
-                name, pattern = token
-                regex = re.compile(pattern)
-                match = regex.match(input_string)
-                if match:
-                    text = match.group(0)
-                    tokens.append((name, text))
-                    input_string = input_string[len(text):]
-                    break
-            if not match:
-                raise ValueError(f"Invalid input: {input_string}")
+
+        # Splitting the source code into "tokens"
+        src_code = self.src_code.split()
+
+        t_index = 0
+
+        # Iterating through "tokens" and dividing into categories
+        while t_index < len(src_code):
+            token = src_code[t_index]
+
+            # Recognise variable declaration
+            if token == "var":
+                tokens.append(['DECLARATION', token])
+
+            # Recognise an integer
+            elif re.match('[0-9]', token):
+                if token[-1] == ";":
+                    tokens.append(['INTEGER', token[:-1]])
+                else:
+                    tokens.append(['INTEGER', token])
+
+            # Recognise a word
+            elif re.match('[a-z]', token) or re.match('[A-Z]', token):
+                if token[-1] == ";":
+                    tokens.append(['IDENTIFIER', token[:-1]])
+                else:
+                    tokens.append(['IDENTIFIER', token])
+
+            # Recognise an operation
+            elif token in "+-=/*":
+                tokens.append(['OPERATOR', token])
+
+            # Recognise the separator, for this case also end of declaration
+            if token[-1] == ";":
+                tokens.append(['SEPARATOR', token[-1]])
+
+            t_index += 1
         return tokens
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
